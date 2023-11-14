dff9b294167d894fe7e69c628950c3e5ca50284e
Danilo Queiroz <daniloqueirozsilva05@gmail.com>
2023-11-13 23:26:33 -0300

    Adição de metodos na PostController

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -56,5 +56,95 @@ namespace Blog.Controllers
                 
 
         }
+
+        [HttpGet("v1/posts/{id:int}")]
+        public async Task<IActionResult> DetailAsync(
+          [FromServices] BlogDataContext context,
+          [FromRoute] int id)
+        {
+
+            try
+            {
+                var count = await context.Posts.AsNoTracking().CountAsync();
+                var post = await context
+                    .Posts
+                    .AsNoTracking()                    
+                    .Include(x => x.Author)
+                    .ThenInclude(x => x.Roles)
+                    .Include(x => x.Category)                   
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (post == null)
+                    return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+               
+                return Ok(new ResultViewModel<Post>(post));
+               
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<List<Post>>("05X06 - Falha interna no servidor"));
+            }
+
+
+        }
+
+        [HttpGet("v1/posts/category/{category}")]
+        public async Task<IActionResult> GetByCategoryAsync(
+          [FromRoute] string category,
+          [FromServices] BlogDataContext context,
+          [FromQuery] int page = 0,
+          [FromQuery] int pageSize = 25)
+        {
+
+            try
+            {
+                var count = await context.Posts.AsNoTracking().Where(x => x.Category.Slug == category).CountAsync();
+                var post = await context
+                    .Posts
+                    .AsNoTracking()
+                    .Include(x => x.Author)                    
+                    .Include(x => x.Category)
+                    .Where(x => x.Category.Slug == category)
+                    .Select(x => new ListPostsViewModel
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        Slug = x.Slug,
+                        LastUpdateDate = x.LastUpdateDate,
+                        Category = x.Category.Name,
+                        Author = $"{x.Author.Name} ({x.Author.Email})"
+                    })
+                    .Skip(page * pageSize)
+                    .Take(pageSize)
+                    .OrderByDescending(x => x.LastUpdateDate)
+                    .ToListAsync(); ;
+
+                if (post == null)
+                    return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+
+                return Ok(new ResultViewModel<dynamic>(
+                   
+                    new 
+                    {
+                        total = count,
+                        page,
+                        pageSize,
+                        post
+                    }));
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<List<Post>>("05X06 - Falha interna no servidor"));
+            }
+
+
+        }
+
+
     }
+
+
 }
